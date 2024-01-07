4628430e074c3a425ed0e54bdcdf5abbdeb35636
raghavigraphics <155875011+raghavigraphics@users.noreply.github.com>
2024-01-07 13:41:48 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HOME</title>
+<style>
+h1 {
+color: rgb(207, 241, 13);
+font-size: 36px;
+}
+p {
+color: rgb(5, 232, 77);
+font-size: 50px;
+text-align: center;
+}
+p1 {
+  text-align: center;
+}
+</style>
+</head>
+<body background="file:///C:/Users/kisha/OneDrive/Desktop/web%20material/photo%20sad%20quetes/4.jpeg">
+<h1 align="center">Broken Heart</h1>
+<p>
+1</br>
+    दील धोके मे था | </br>
+  ओैर धोकेबाज दिल मे थे  ||</br>
+  <br>
+  <br>
+   2</br>
+     |मन तुटण्याने जर<br>
+     आवाज झाला असता<br>
+     तर देवाशप्पथ आज<br>
+     खूप मोठा आवाज झाला असता. <br>
+  ||</br>
+  <br>
+  3</br>
+  आयुष्यात एकदाच आलीस<br>
+   पण
+  सर्व लाईफ<br>
+   तुझ्या आठवणीने busy करून गेली.  ||</br>
+</p>
+<center><img src="file:///C:/Users/kisha/OneDrive/Desktop/web%20material/photo%20sad%20quetes/2.jpg" alt="sorry" height="300px" width="300px" border="5px"></center>
+<p>
+  4</br>
+  रडल्यामुळे कधी कोणी आपले होत नसते<br>
+  आणि तसेही जे आपले असतात<br>
+  ते रडू थोडी देतात</br>
+    <br>
+    <br>
+     5</br>
+       |मन तुटण्याने जर<br>
+       आवाज झाला असता<br>
+       तर देवाशप्पथ आज<br>
+       खूप मोठा आवाज झाला असता. <br>
+    ||</br>
+    6</br>
+    जशी प्रत्येक मुलगी  <br>
+    मुलाच्या पैश्यावार प्रेम करत नाही <br>
+    तसेच प्रत्येक मुलगा  <br>
+    मुलीच्या शरीरावर प्रेम करत नाही. <br>
+    <br>
+
+  </p>
+  <center><img src="file:///C:/Users/kisha/OneDrive/Desktop/web%20material/photo%20sad%20quetes/3.jpg" alt="sorry" height="300px" width="300px" border="3px" ></center>
+  <p>
+  7</br>
+  नॅायलोन च्या दोरीवर  <br>
+आणी हसणारया पोरीवर  कधीच  <br>
+विश्वास ठेउू नये... <br>
+    
+  </br>
+  8</br>
+जेवढं माझ्याकडे जळून पाहशील<br>
+तेवढंच माझ्याकडे वळून पाहशील<br>
+<br>
+    
+  9<br>
+असेच होते म्हणायचे तर अशी अचानक भ्यालिस का? <br>
+अर्ध्या वाटेवरती जाऊन पुन्हा परत तू आलीस का?<br>
+<br>
+
+10<br>
+
+इतकेही प्रेम करु नये कि प्रेम हेच जीवन होईल कारण <br>
+प्रेमभंग झाल्यावर जीवंतपणी मरण येईल<br>
+<center><img src="file:///C:/Users/kisha/OneDrive/Desktop/web%20material/photo%20sad%20quetes/3.jpg" alt="sorry" height="300px" width="300px" border="3px" ></center>
+  </p>
+  <p>
+<br>
+
+11<br>
+कसं काय विसरून जाऊ तिला..</br>
+मरण माणसाला येतं,</br>
+आठवणींना नाही..</br>
+
+</br>
+</br>
+12<br>
+|मन तुटण्याने जर<br>
+आवाज झाला असता<br>
+तर देवाशप्पथ आज<br>
+खूप मोठा आवाज झाला असता. <br>
+||</br>
+</p>
+
+</body>
+</html>
